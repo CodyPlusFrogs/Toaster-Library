36b6d24ab63049635630e4b4ec3f60d9d8c31875
Peter Szczeszynski <PeterTheearthling@gmail.com>
2020-03-09 13:26:34 -0400

    gf test

---
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.test;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.toasterlibrary.ToasterMessage;
+
+
+public class MainActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ToasterMessage
+    }
+}
